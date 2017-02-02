832f4fb1663b31888dc4966f5500c6da3721daf7
Stan Grishin <stangri@melmac.net>
2017-02-02 10:12:19 -0800

    magisk v10-compatible version 10.0.0

--- config.sh
+++ config.sh
@@ -34,10 +34,10 @@ MODID=Tethering_Enabler
 AUTOMOUNT=false
 
 # Set to true if you need to load system.prop
-PROPFILE=true
+PROPFILE=false
 
 # Set to true if you need post-fs-data script
-POSTFSDATA=false
+POSTFSDATA=true
 
 # Set to true if you need late_start service script
 LATESTARTSERVICE=false
